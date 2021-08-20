8e47e7918532923188ee73e974e6e0e39fac9acd
Artemleonenkoav <leonenkoavfin@gmail.com>
2021-08-20 13:58:01 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+    public static void print3(String text) {
+        //напишите тут ваш код
+for (int i=0;i < 3 ; i++)
+    System.out.print(text+" ");
+    }
+
+    public static void main(String[] args) {
+        print3("window");
+        print3("file");
+    }
+}
